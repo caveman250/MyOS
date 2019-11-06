bd9fa4e7976f34ba624ac4b212e60108e6a2b13a
caveman250 <jason.wood@hotmail.com.au>
2019-11-06 17:41:36 +1100

    removed a required line from my last commit oops

--- run_os.sh
+++ run_os.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+echo start
 echo finish #this is just here so VSCode wont wait for qemu to exit before it starts debugging.
 qemu-system-i386 -S -s -cdrom myos.iso -fda floppy.img -serial file:./serial.log -boot d
